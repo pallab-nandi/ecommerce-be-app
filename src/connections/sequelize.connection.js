413bcd440821c8b67f5fac5d626d4accd391984e
Pallab Nandi <pallabnandi6+dev@gmail.com>
2022-12-05 20:38:44 +0530

    updated

--- src/connections/sequelize.connection.js
+++ src/connections/sequelize.connection.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV || 'production'
 const { Sequelize } = require('sequelize');
 const dbConfig = require('./../configs/db.config')[env];
 
